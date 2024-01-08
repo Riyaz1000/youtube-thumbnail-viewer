d254317edd8df49c8c7d6da0555d5b34710220af
Riyaz <91373561+Riyaz1000@users.noreply.github.com>
2024-01-08 23:05:01 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  // base: '/YOUR_BASE/'
+  base: "/youtube-thumbnail-viewer/",
   //   build: {
   //     rollupOptions: {
   //       input: {
